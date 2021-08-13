466538b521a6a0ce888a6090e170e28b0171655c
Jennas-Lee <leezeunjun@gmail.com>
2021-08-13 10:53:14 +0900

    Add healthcheck

--- app.py
+++ app.py
@@ -10,14 +10,19 @@ async def accept(websocket, path):
         data = await websocket.recv()
         try:
             json_data = json.loads(data)
-            image = Image(websocket, json_data['image'])
-            await image.main()
+            if json_data['process'] == 0:  # healthcheck
+                await websocket.send('{"progress": 0, "message": "연결 성공"}')
+            elif json_data['process'] == 1:  # process cv & ocr
+                image = Image(websocket, json_data['image'])
+                await image.main()
+            else:
+                pass
         except json.decoder.JSONDecodeError:
             pass
 
 
 def main():
-    start_server = websockets.serve(accept, '192.168.137.1', 5000)
+    start_server = websockets.serve(accept, '127.0.0.1', 5000)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
 
